29ff166de9179f5a8507eff7ccd573b4846e9014
Geoffrey BOURGUIGNON <geoffrey.bourguignon@idci-consulting.fr>
2011-10-26 16:28:21 +0200

    Add result page and route

---
+++ apps/frontend/modules/result/actions/actions.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * result actions.
+ *
+ * @package    sf_sandbox
+ * @subpackage result
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class resultActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+
+  public function executeShow(sfWebRequest $request)
+  {
+    
+  }
+}
---
+++ apps/frontend/modules/result/templates/showSuccess.php
@@ -0,0 +1,25 @@
+<table class="homepage">
+  <tr class="head">
+    <th class="url">Adresse du site</th><th>Graphisme</th><th>Ergonomie</th><th>...Autres categs ...</th><th>Note Globale</th>
+  </tr>
+    <tr>
+      <td class="site">
+        URL SITE
+      </td>
+      <td>
+        10/40
+      </td>
+      <td>
+        20/40
+      </td>
+      <td>
+      ...
+      </td>
+      <td>
+       30/80
+      </td>
+    </tr>
+    <tr>
+      <td>Possibilité de classer par note de graphisme, par note d'ergonomie, etc... et par note globale</td>
+    </tr>
+</table>

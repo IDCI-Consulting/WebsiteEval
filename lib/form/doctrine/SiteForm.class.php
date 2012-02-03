5214c947ceb15a4b44509cc90a03d8195d088682
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2012-02-03 11:30:31 +0100

    Add missing functions

--- lib/form/doctrine/SiteForm.class.php
+++ lib/form/doctrine/SiteForm.class.php
@@ -12,5 +12,6 @@ class SiteForm extends BaseSiteForm
 {
   public function configure()
   {
+    unset($this['created_at'], $this['updated_at']);
   }
 }
--- lib/model/doctrine/Question.class.php
+++ lib/model/doctrine/Question.class.php
@@ -12,4 +12,8 @@
  */
 class Question extends BaseQuestion
 {
+  public function setText($text)
+  {
+    parent::_set('text', htmlspecialchars($text));
+  }
 }
